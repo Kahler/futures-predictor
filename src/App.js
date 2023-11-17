34928c4d72623cfb80b5da64697ed69aa05b9df8
Kaehler <nils.kaehler@gmail.com>
2023-11-18 00:49:49 +0100

    add chart

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import GraphChart from './components/GraphChart';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <GraphChart />
       </header>
     </div>
   );
