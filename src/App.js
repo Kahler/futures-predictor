6e1acb75518a43b16f02b4cab1a3508057277c85
Tom Quast <tomquast89@gmail.com>
2023-11-18 14:30:15 +0100

    failing api

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import LineChart from './components/LineChart';
 function App() {
 
   const [stockMarket, setStockMarket] = useState(null);
+  const [news, setNews] = useState(null);
 
   useEffect(() => {
     fetch("https://financialmodelingprep.com/api/v3/profile/AAPL?apikey=dc376cb55d5e600338867ca3e24b3728")
@@ -19,8 +20,20 @@ function App() {
         }
       })
       .catch(err => console.log(err));
-  }, [])
 
+      fetch("https://via.ritzau.dk/json/v2/releases")
+      .then(res => {
+        if (res.ok) {
+          res.json().then(data => {
+            setNews(data);
+            console.log(data);
+          }).catch(r => console.log(r));
+        } else {
+          throw new Error('Network response was not ok.');
+        }
+      })
+      .catch(err => console.log(err));
+  }, [])
 
   return (
     <div className="App">
